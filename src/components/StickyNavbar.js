6a4b943194e3811fb97fa7d70bc44460dbd51482
RADOM KHOEM <138679281+roldophe@users.noreply.github.com>
2024-04-02 16:57:08 +0700

    Update Collapse

--- src/components/StickyNavbar.js
+++ src/components/StickyNavbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {
 
-  MobileNav,
+  Collapse,
   Typography,
   Menu,
   MenuHandler,
@@ -341,9 +341,9 @@ export function StickyNavbar() {
                     Login
                 </Typography>
         </div>
-        <MobileNav open={isNavOpen} className="overflow-scroll">
+        <Collapse open={isNavOpen} className="overflow-scroll">
           <NavList />
-        </MobileNav>
+        </Collapse>
       </div>
     </header>
   );
