0cb111858d8dbfcdcc864c2051c0958654d7de82
RADOM KHOEM <138679281+roldophe@users.noreply.github.com>
2024-10-19 16:51:46 +0700

    update trending

--- src/redux/actions/TrendingAction.jsx
+++ src/redux/actions/TrendingAction.jsx
@@ -4,7 +4,7 @@ import { ActionTypes } from "./ActionTypes"
 
 export const fetch_all_trending=() =>{
     return (dispatch) => {
-        fetch(`${Base_Url}/trending/all/day?api_key=${api_key}&language=en-US&page=1`)
+        fetch(`${Base_Url}/trending/movie/week?api_key=${api_key}&language=en-US&page=1`)
             .then(resp => resp.json())
             .then(res => dispatch({
                 type: ActionTypes.FETCH_TRENDING,
